d1684919df1a4d76a0616780401686c3f40928b4
JhonatanEstabile <jhonatanestabeli@hotmail.com>
2021-04-11 14:28:06 -0300

    fix app name

--- lib/mainCombustivel.dart
+++ lib/mainCombustivel.dart
@@ -2,10 +2,10 @@ import 'package:exercicios_flutter_fib/components/resultadoCombustivel.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(LoginApp());
+  runApp(CombustivelApp());
 }
 
-class LoginApp extends StatelessWidget {
+class CombustivelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
